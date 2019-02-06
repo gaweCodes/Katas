bd4f7b2199fad4f93282da8940ba84fefc1d6583
Gabriel Weibel <g.weibel@rey-automation.ch>
2019-02-06 21:32:36 +0100

    OddNumbersSorting

---
+++ Katas/SortOddNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SortOddNumbers
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            foreach (var i in SortArray(new[] {1, 5, 3, 2, 9, 7}))
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+        public static int[] SortArray(int[] array)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0 || array[i] == 0)
+                    continue;
+
+                for (var j = (i + 1); j < array.Length; j++)
+                {
+                    if (array[j] % 2 == 0 || array[i] == 0) continue;
+                    if (array[i] <= array[j]) continue;
+                    var temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+            return array;
+        }
+    }
+}
